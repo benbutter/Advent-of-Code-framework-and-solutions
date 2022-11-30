d6156ab35e94e4c848be7175c9c74fbb691f7447
ben butterfield <ben.butterfield@gmail.com>
2022-11-30 18:57:57 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AOC_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
