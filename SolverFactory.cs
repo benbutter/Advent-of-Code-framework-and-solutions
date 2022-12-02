56f538894842ec75ec9221d023f911649b5e252b
ben butterfield <ben.butterfield@gmail.com>
2022-12-02 09:12:02 +0000

    .

--- SolverFactory.cs
+++ SolverFactory.cs
@@ -9,31 +9,45 @@ namespace AOC_2022
     {
         public ISolver GetSolver(string suffix)
         {
-            string typename = $"AOC_2022._{suffix}.Solver{suffix}";
-            
-            Type type = Type.GetType(typename);
-       
-            if (type == null)
-            {
-                throw new Exception("Type not found.");
-            }
-            var instance = Activator.CreateInstance(type);
+            Type type = GetSolverType(suffix);
 
-            ISolver solver = (ISolver)instance;
+            ISolver solver = CreateSolverInstance(type);
 
             solver.SetInput(GetInput(suffix));
 
             return solver;
-            
+
+        }
+
+        private static Type GetSolverType(string suffix)
+        {
+            string typename = $"AOC_2022._{suffix}.Solver{suffix}";
+
+            Type type = Type.GetType(typename);
+
+            if (type == null)
+            {
+                throw new Exception("Type not found.");
+            }
+
+            return type;
+        }
+
+        private static ISolver CreateSolverInstance(Type type)
+        {
+            var instance = Activator.CreateInstance(type);
+
+            ISolver solver = (ISolver)instance;
+            return solver;
         }
 
         private StreamReader GetInput(string suffix)
         {
 
             string path = $"C:\\Users\\benbu\\source\\repos\\AOC-2022\\{suffix}\\input.txt";
-            if (File.Exists(path))
+            if (!File.Exists(path))
             {
-                Console.WriteLine("Y");
+                throw new Exception("File not found.");
             }
 
             var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
