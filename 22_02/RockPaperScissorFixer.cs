4a63f6db9f36c89cee2f5ebc1f79eb13f6a9ffdb
ben butterfield <ben.butterfield@gmail.com>
2022-12-02 12:47:58 +0000

    2202 part 2 fixer created

---
+++ 22_02/RockPaperScissorFixer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC_2022._22_02
+{
+    class RockPaperScissorFixer
+    {
+        public char GetOptionToFixResult(char opponetsGo, char resultToFixTo)
+        {
+
+            //LOSE
+            if (resultToFixTo == 'X')
+            {
+                if (opponetsGo == 'A')
+                {
+                    return 'Z';
+                }
+                if (opponetsGo == 'B')
+                {
+                    return 'X';
+                }
+                if (opponetsGo == 'C')
+                {
+                    return 'Y';
+                }
+            }
+
+            //draw
+            if (resultToFixTo == 'Y')
+            {
+                if (opponetsGo == 'A')
+                {
+                    return 'X';
+                }
+                if (opponetsGo == 'B')
+                {
+                    return 'Y';
+                }
+                if (opponetsGo == 'C')
+                {
+                    return 'Z';
+                }
+            }
+                
+            //WINS
+             if (resultToFixTo == 'Z')
+            {
+                if (opponetsGo == 'A')
+                    {
+                    return 'Y';
+                }
+
+                if (opponetsGo == 'B')
+                {
+                    return 'Z';
+                }
+
+                if (opponetsGo == 'C')
+                {
+                    return 'X';
+                }
+            }
+            return 'Q';
+        }
+    }
+}
---
+++ 22_02/RockPaperScissorFixerTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AOC_2022._22_02
+{
+    [TestClass]
+    public class RockPaperScissorFixerTests
+    {
+        //          Rock Paper Scissors
+        //Opponents A       B       C
+        //you       X       Y       Z
+
+        //         lose   draw    win
+        //result     X       Y       Z
+
+
+        //Losses
+        [TestMethod]
+        public void GetOptionToFixResult_A_X_ReturnsZ()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('A', 'X');
+
+            Assert.AreEqual('Z', result);
+        }
+
+        [TestMethod]
+        public void GetOptionToFixResult_B_X_ReturnsX()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('B', 'X');
+
+            Assert.AreEqual('X', result);
+        }
+
+
+        [TestMethod]
+        public void GetOptionToFixResult_C_X_ReturnsY()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('C', 'X');
+
+            Assert.AreEqual('Y', result);
+        }
+    
+
+
+        //Draws
+        [TestMethod]
+        public void GetOptionToFixResult_A_Y_ReturnsX()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('A', 'Y');
+
+            Assert.AreEqual('X', result);
+        }
+
+        [TestMethod]
+        public void GetOptionToFixResult_B_Y_ReturnsY()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('B', 'Y');
+
+            Assert.AreEqual('Y', result);
+        }
+
+        [TestMethod]
+        public void GetOptionToFixResult_C_Y_ReturnsZ()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('C', 'Y');
+
+            Assert.AreEqual('Z', result);
+        }
+
+        //Wins
+        [TestMethod]
+        public void GetOptionToFixResult_A_Z_ReturnsY()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('A', 'Z');
+
+            Assert.AreEqual('Y', result);
+        }
+
+        [TestMethod]
+        public void GetOptionToFixResult_B_Z_ReturnsZ()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('B', 'Z');
+
+            Assert.AreEqual('Z', result);
+        }
+
+        [TestMethod]
+        public void GetOptionToFixResult_C_Z_ReturnsX()
+        {
+            var fixer = new RockPaperScissorFixer();
+
+            var result = fixer.GetOptionToFixResult('C', 'Z');
+
+            Assert.AreEqual('X', result);
+        }
+    }
+}
