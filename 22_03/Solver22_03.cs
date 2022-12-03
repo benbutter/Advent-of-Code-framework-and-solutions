e8724fc6b1914bb2e71e451d80756966568b5611
ben butterfield <ben.butterfield@gmail.com>
2022-12-03 21:21:31 +0000

    tidied

--- 22_03/Solver22_03.cs
+++ 22_03/Solver22_03.cs
@@ -19,26 +19,11 @@ namespace AOC_2022._22_03
         public int GetSolution()
         {
             int total = 0;
-            string line;
-            
-            
-            int count = 0;
+        
             var priorityFinder = new PriorityFinder();
 
             while (!input.EndOfStream)
             {
-
-                // var halver = new StringHalver(); part 1
-                //   var charsFinder = new CommonCharsFinder(); //part 1
-
-                //   var halvesArray = halver.HalveString(line); part 1
-                //   var chars = charsFinder.FindCommonChars(halvesArray[0], halvesArray[1]); //part 1
-
-                // lines[count] = input.ReadLine();
-                //  if (count % 3 == 0)
-                //  {
-
-                //  string[] lines = new string[3];
 
                 linesToCheck.Add(input.ReadLine());
                 if (linesToCheck.Count == 3)
@@ -58,7 +43,7 @@ namespace AOC_2022._22_03
 
             var priorityFinder = new PriorityFinder();
             return priorityFinder.GetPriority(chars[0]);
-          // return 0;
+        
         }
     }
 }
