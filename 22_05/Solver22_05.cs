312c925a145b552761803b434c220447e75c89b8
ben butterfield <ben.butterfield@gmail.com>
2022-12-06 16:01:48 +0000

    05 part 1 done - lots of tidying to do

--- 22_05/Solver22_05.cs
+++ 22_05/Solver22_05.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AOC_2022._22_05
 {
@@ -17,6 +18,8 @@ namespace AOC_2022._22_05
         {
             bool stackingPhaseOver = false;
             string line;
+            string returnVal = "";
+
             while (!input.EndOfStream)
             {
                 line = input.ReadLine();
@@ -39,7 +42,8 @@ namespace AOC_2022._22_05
                 }
                 if (!stackingPhaseOver)
                 {
-                    CreateStacksHardCodedForTestInput(line);
+                  //  CreateStacksHardCodedForTestInput(line);
+                    CreateStacksHardCodedForProdInput(line);
                 }
                 else
                 {   if (line.Length > 0)
@@ -49,21 +53,45 @@ namespace AOC_2022._22_05
 
             for (int i = 0; i < crates.Count; i++)
             {
-               Console.WriteLine(crates[i]);
+                returnVal += GetLastCharOfEachCrate(i);
             }
-            return "";
+
+            return returnVal;
+        }
+
+        private string GetLastCharOfEachCrate(int i)
+        {
+            return crates[i][crates[i].Length - 1].ToString();
         }
 
         private void MoveCrates(string line)
         {
-            int numberToMove = Int32.Parse( line[5].ToString());
-            int moveFrom = Int32.Parse(line[12].ToString()) -1;
-            int moveTo = Int32.Parse(line[17].ToString()) -1;
+            /*   int numberToMove = Int32.Parse( line[5].ToString());
+               int moveFrom = Int32.Parse(line[12].ToString()) -1;
+               int moveTo = Int32.Parse(line[17].ToString()) -1;
+            
+
+            //string input = ;
+            // Split on one or more non-digit characters.
+           
+            foreach (string value in numbers)
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    int i = int.Parse(value);
+                    Console.WriteLine("Number: {0}", i);
+                }
+            }*/
+
+            string[] numbers = Regex.Split(line, @"\D+");
+            int numberToMove = Int32.Parse(numbers[1]);
+            int moveFrom = Int32.Parse(numbers[2])-1;
+            int moveTo = Int32.Parse(numbers[3])-1;
 
             for (int i = 0; i < numberToMove; i++)
             {
                 //move last char
-                crates[moveTo] = crates[moveTo] + crates[moveFrom][crates[moveFrom].Length -1];
+                 crates[moveTo] = crates[moveTo] + crates[moveFrom][crates[moveFrom].Length -1];
 
                 //remove last char
                 crates[moveFrom] = crates[moveFrom].Remove(crates[moveFrom].Length - 1, 1);
@@ -87,6 +115,34 @@ namespace AOC_2022._22_05
             }
         }
 
+        private void CreateStacksHardCodedForProdInput(string line)
+        {
+            if (crates.Count == 0)
+            {
+                crates.Add(line[1].ToString());
+                crates.Add(line[5].ToString());
+                crates.Add(line[9].ToString());
+                crates.Add(line[13].ToString());
+                crates.Add(line[17].ToString());
+                crates.Add(line[21].ToString());
+                crates.Add(line[25].ToString());
+                crates.Add(line[29].ToString());
+                crates.Add(line[33].ToString());
+            }
+            else
+            {
+                crates[0] = crates[0] + (line[1].ToString());
+                crates[1] = crates[1] + (line[5].ToString());
+                crates[2] = crates[2] + (line[9].ToString());
+                crates[3] = crates[3] + (line[13].ToString());
+                crates[4] = crates[4] + (line[17].ToString());
+                crates[5] = crates[5] + (line[21].ToString());
+                crates[6] = crates[6] + (line[25].ToString());
+                crates[7] = crates[7] + (line[29].ToString());
+                crates[8] = crates[8] + (line[33].ToString());
+            }
+        }
+
         private void AddToCrates(string crateName, int crateStack)
         {
            if (crates.Count < crateStack)
@@ -100,8 +156,6 @@ namespace AOC_2022._22_05
         }
         static string ReverseString(string s)
         {
-            // Convert to char array, then call Array.Reverse.
-            // ... Finally use string constructor on array.
             char[] array = s.ToCharArray();
             Array.Reverse(array);
             return new string(array);
