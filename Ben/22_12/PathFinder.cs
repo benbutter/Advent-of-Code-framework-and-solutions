ec1d956b7a053840cda0ff181d794d8c1cbdbaea
ben butterfield <ben.butterfield@gmail.com>
2022-12-14 19:48:10 +0000

    .

---
+++ Ben/22_12/PathFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC_2022._22_12
+{
+    class PathFinder
+    {
+        //if multiple options preference is u,r,d,l
+
+        List<string> previousPaths = new List<string>();
+        List<string> map;
+
+        string currentPath;
+        int currentX = 0;
+        int currentY = 0;
+        public void createPaths()
+        {
+           
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ Ben/22_12/Solver22_12.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC_2022._22_12
+{
+    class Solver22_12
+    {
+    }
+}
