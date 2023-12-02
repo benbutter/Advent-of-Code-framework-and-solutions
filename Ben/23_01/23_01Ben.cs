d85f018db03bb9a91deb86da236bf404fdcf433d
Ben Butterfield <ben.butterfield@gmail.com>
2023-12-02 23:19:14 +0000

    23 01 done part 1

---
+++ Ben/23_01/23_01Ben.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2022._23_01
+{
+    public class _23_01Ben : ISolver
+    {
+        List<int> ints;
+        public _23_01Ben()
+        {
+            ints = new List<int>();   
+        }
+        public string GetSolution(StreamReader streamReader)
+        {
+            while (!streamReader.EndOfStream)
+            {
+                int firstNumber = 0;
+                int secondNumber = 0;
+                
+                var line = streamReader.ReadLine();
+                
+                foreach (var item in line)
+                {
+                    if (Int32.TryParse(item.ToString(),out firstNumber))
+                    {
+                        break;
+                    }
+
+                }
+
+               var reversedLine = line.Reverse();
+
+
+                foreach (var item in reversedLine)
+                {
+                    if (Int32.TryParse(item.ToString(), out secondNumber))
+                    {
+                        break;
+                    }
+
+                }
+                
+                ints.Add(Int32.Parse(firstNumber.ToString() + secondNumber.ToString()));
+            }
+
+            int total = 0;
+
+            foreach (var item in ints)
+            {
+                total += item;
+            }
+
+            return total.ToString();
+        }
+    }
+}
---
+++ Ben/23_02/Solver23_02Ben.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2022._23_02
+{
+    internal class Solver23_02Ben : ISolver
+    {
+        Dictionary<string, int> colourTotal;
+        List<int> validGames;
+        int gameNumber;
+        int total;
+        public Solver23_02Ben()
+        {
+            colourTotal = new Dictionary<string, int>();
+
+            colourTotal.Add("Blue", 0);
+            colourTotal.Add("Red", 0);
+            colourTotal.Add("Green", 0);
+
+            validGames = new List<int>();
+
+            gameNumber = 0;
+            total = 0;
+        }
+        public string GetSolution(StreamReader streamReader)
+        {
+            while (!streamReader.EndOfStream)
+            {
+                var line = streamReader.ReadLine();
+                gameNumber++;
+
+                var reversedLine = line.Reverse();
+                var splitLines = reversedLine.ToString().Split(" ");
+
+
+            }
+
+            return "0";
+        }
+
+
+
+        private void ResetTotals()
+        {
+            colourTotal["Blue"] = 0;
+            colourTotal["Red"] = 0;
+            colourTotal["Green"]= 0;
+        }
+
+        private bool IsGameVaid()
+        {
+            return  colourTotal["Blue"] <= 14 &
+                    colourTotal["Red"] <= 12 &
+                    colourTotal["Green"] <= 13;
+        }
+    }
+}
