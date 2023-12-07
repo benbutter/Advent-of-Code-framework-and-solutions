980391ea55008eaf3b1649313db2b3df575f7447
Ben Butterfield <ben.butterfield@gmail.com>
2023-12-07 21:44:49 +0000

    23_06 part 1 done

---
+++ Ben/23_06/DistanceCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2022._23_06
+{
+    public class DistanceCalculator
+    {
+        public List<int> GetDistances (int maxTime)
+        { 
+            List<int> result = new List<int>();
+
+            for (int i = 0; i <= maxTime; i++) 
+            { 
+                int timeLeft = maxTime - i;
+
+                int distance = i * timeLeft;
+
+                result.Add(distance);
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ Ben/23_06/Solver23_06Ben.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2022._23_06
+{
+    public class Solver23_06Ben : ISolver
+    {
+        List<int> distances;
+        List<int> times;
+        int raceTotalWinners = 0;
+        int returnTotal = 0;
+
+        public Solver23_06Ben()
+        {
+             distances = new List<int>();
+             times = new List<int>();
+        }
+        
+
+        public string GetSolution(StreamReader streamReader)
+        {
+            SetupTimesAndDistances(streamReader);
+
+            DistanceCalculator calc = new DistanceCalculator();
+
+            for (int i = 0; i < times.Count; i++) 
+            {
+                var possibleDistancesForRace = calc.GetDistances(times[i]);
+
+                foreach (var possibleDistance in possibleDistancesForRace)
+                {
+                    if (possibleDistance > distances[i])
+                    {
+                        raceTotalWinners++;
+                    }
+                }
+
+                if (returnTotal == 0)
+                {
+                    returnTotal = raceTotalWinners;
+                }
+                else
+                {
+                    returnTotal = returnTotal * raceTotalWinners;
+                }
+                raceTotalWinners = 0;
+            }
+          
+            return returnTotal.ToString();
+        }
+
+        private void SetupTimesAndDistances(StreamReader streamReader)
+        {
+            var line = streamReader.ReadLine();
+
+            var unsanitizedTimes = line.Split(' ');
+
+            var line2 = streamReader.ReadLine();
+
+            var unsanitizedDistances = line2.Split(" ");
+
+
+
+            foreach (var s in unsanitizedTimes)
+            {
+                int number = 0;
+
+                if (Int32.TryParse(s, out number))
+                {
+                    times.Add(number);
+                }
+            }
+
+            foreach (var s in unsanitizedDistances)
+            {
+                int number = 0;
+
+                if (Int32.TryParse(s, out number))
+                {
+                    distances.Add(number);
+                }
+            }
+        }
+    }
+}
