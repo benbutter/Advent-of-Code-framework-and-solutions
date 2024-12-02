6d387fb9ab3779f98cbade3848fb4a05a93c7abc
Ben Butterfield <ben.butterfield@gmail.com>
2024-12-02 22:34:02 +0000

    .

--- Ben/24_01/Solver24_01Ben.cs
+++ Ben/24_01/Solver24_01Ben.cs
@@ -1,30 +1,46 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AOC_2022._24_07
 {
     public class Solver24_01Ben : ISolver
     {
+        public int intToMatchFromListOne;
+
         public string GetSolution(StreamReader streamReader)
         {
 
             List<int> listOne = new List<int>();
             List<int> listTwo = new List<int>();
             List<int> differences = new List<int>();
+            int count = 0;
 
             PopulateBothLists(streamReader, listOne, listTwo);
 
             listOne.Sort();
             listTwo.Sort();
 
-            PopulateDifferences(listOne, listTwo, differences);
+            //part 1
+           // PopulateDifferences(listOne, listTwo, differences);
 
-            return differences.Sum().ToString();
-            
+           // return differences.Sum().ToString();
+            //part 2
+
+            foreach (int i in listOne)
+            {
+                var matches = GetNumberOfMatches(i, listTwo);
+
+                count = count + (i * matches);
+            }
+
+            return count.ToString();
         }
 
         private static void PopulateDifferences(List<int> listOne, List<int> listTwo, List<int> differences)
@@ -34,6 +50,24 @@ namespace AOC_2022._24_07
                 differences.Add(Math.Abs((listOne[i] - listTwo[i])));
             }
         }
+
+        private static int GetNumberOfMatches(int intToMatch, List<int> list)
+        {
+            //  intToMatchFromListOne = intToMatch;
+            //List<int> Result = new List<int>(list.FindAll(Predicate));
+            int count;
+            count = 0;
+            IEnumerable matches = list.Where(x => x == intToMatch);
+            
+            foreach (var match in matches)
+            {
+                count++;
+            }
+            return count;
+        }
+
+      
+
 
         private  void PopulateBothLists(StreamReader streamReader, List<int> listOne, List<int> listTwo)
         {
